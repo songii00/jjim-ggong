536c47788e65d0ef5396729c6e58464e4ca1fb97
songii00 <wks4j1004@gmail.com>
2020-06-06 22:50:49 +0900

    init

---
+++ src/main/java/com/api/jjimggong/JjimggongApplication.java
@@ -0,0 +1,13 @@
+package com.api.jjimggong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JjimggongApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JjimggongApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/api/jjimggong/JjimggongApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.jjimggong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JjimggongApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
